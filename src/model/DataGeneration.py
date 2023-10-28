74bc6219c3106ee9d3d8f004f3cf8b99de81d8d0
fairytree <49176519@qq.com>
2023-10-29 07:59:59 +0800

    Auto-generate images and labels, and save to files

--- src/model/DataGeneration.py
+++ src/model/DataGeneration.py
@@ -2,9 +2,11 @@ import cv2
 import numpy as np
 from PIL import ImageFont, ImageDraw, Image
 import os
+import random
 
 
-def placeShape(img, shape, coord, size, color):
+def placeShape(img_original, shape, coord, size, color):
+    img = img_original.copy()
     if shape == "circle":
         return cv2.circle(img, coord, int(size / 2), color, cv2.FILLED)
     elif shape == "semicircle":
@@ -28,7 +30,7 @@ def placeShape(img, shape, coord, size, color):
             img, center, axes, angle, startAngle, endAngle, color, cv2.FILLED
         )
     elif shape == "triangle":
-        triangle_pts = np.array(
+        vertices = np.array(
             [
                 (int(coord[0] - (size / 2)), int(coord[1] + (size / 2))),
                 (coord[0], int(coord[1] - (size / 2))),
@@ -36,15 +38,16 @@ def placeShape(img, shape, coord, size, color):
             ],
             np.int32,
         )
-        return cv2.fillPoly(img, [triangle_pts], color)
     elif shape == "rectangle":
-        return cv2.rectangle(
-            img,
-            (int(coord[0] - (size / 2)), int(coord[1] - (size / 2))),
-            (int(coord[0] + (size / 2)), int(coord[1] + (size / 2))),
-            color,
-            cv2.FILLED,
-        )
+        vertices = np.array(
+            [
+               [int(coord[0] - (size / 2)), int(coord[1] - (size / 2))],
+               [int(coord[0] + (size / 2)), int(coord[1] - (size / 2))],
+               [int(coord[0] + (size / 2)), int(coord[1] + (size / 2))],
+               [int(coord[0] - (size / 2)), int(coord[1] + (size / 2))],
+            ],
+            np.int32,
+        )        
     elif shape == "pentagon":
         # Calculate the coordinates of the pentagon vertices
         vertices = np.array(
@@ -57,9 +60,6 @@ def placeShape(img, shape, coord, size, color):
             ],
             np.int32,
         )
-
-        # Draw the pentagon on the image
-        return cv2.fillPoly(img, [vertices], color=color)
     elif shape == "star":
         outter_vertices = np.array(
             [
@@ -85,11 +85,48 @@ def placeShape(img, shape, coord, size, color):
         vertices[0::2] = outter_vertices
         vertices[1::2] = inner_vertices
 
-        # Draw the star on the image
-        return cv2.fillPoly(img, [vertices], color=color)
+    # find the boinding box
+    [highest, lowest, leftmost, rightmost] = findBoundingBox(img, vertices, shape)
+    
+    # Draw the shape on the image
+    return cv2.fillPoly(img, [vertices], color=color), [highest, lowest, leftmost, rightmost]
 
 
-
+def findBoundingBox(img, vertices, shape):
+    # find the highest, lowest, leftmost and rightmost points
+    
+    if(shape in ["circle", "semicircle", "quarter circle"]):
+        pass # TODO
+    else:    
+        highest = img.shape[0]
+        lowest = 0
+        leftmost = img.shape[1]
+        rightmost = 0
+        for vertex in vertices:
+            if vertex[0] <= leftmost:
+                leftmost = vertex[0]
+            if vertex[0] >= rightmost:
+                rightmost = vertex[0]
+            if vertex[1] <= highest:
+                highest = vertex[1]
+            if vertex[1] >= lowest:
+                lowest = vertex[1]
+            
+    if(highest < 0):
+        highest = 0
+    if(lowest > img.shape[0]):
+        lowest = img.shape[0]
+    if(leftmost < 0 ):
+        leftmost = 0
+    if(rightmost > img.shape[1]):
+        rightmost = img.shape[1]
+        
+    highest /= img.shape[0]
+    lowest /= img.shape[0]
+    leftmost /= img.shape[1]
+    rightmost /= img.shape[1]
+    print("the highest (y), lowest (y), leftmost (x) and rightmost (x) coords (normalized): ", highest, lowest, leftmost, rightmost)
+    return [highest, lowest, leftmost, rightmost]
 
 def putText(img, text, coord, size):
     # Convert the image to RGB (OpenCV uses BGR)
@@ -118,24 +155,69 @@ def putText(img, text, coord, size):
     # Get back the image to OpenCV
     return cv2.cvtColor(np.array(pil_im), cv2.COLOR_RGB2BGR)
 
-def createImage(bg_path, shape, coord, size, color, text):
+def createImage(img_original, shape, coord, size, color, text):
     """
     create training data image
     """
-    img = Image.open(bg_path)
-    img = np.asarray(img)
-    img = placeShape(img, shape, coord, size, color)
+    img, [highest, lowest, leftmost, rightmost] = placeShape(img_original, shape, coord, size, color[1])
     img = putText(img, text, coord, size)
     img = cv2.GaussianBlur(img, (7,7), 0)
+
+    # write image to the file
+    os.chdir("../images")  # change to the right directory
+    image_file = str(color[0]) + "_" + str(shape) + ".jpg"     # create image file
+    cv2.imwrite(image_file, img)
+
+    # create corresponding label file
+    os.chdir("../labels")
+    addLabel(highest, lowest, leftmost, rightmost, color[0], shape)
+    
     return img
 
 
-size = 100
-coord = (300, 150)
-color = (255, 0, 0)
-bg_path = "src/model/resources/backgrounds/pavement3.jpg"
-img = createImage(bg_path, "pentagon", coord, size, color, "A")
-img = Image.fromarray(np.uint8(img))
-
-img.show()
-cv2.waitKey(0)
+def addLabel(highest, lowest, leftmost, rightmost, color, shape):
+    # add labels
+    label = open(str(color) + "_" + str(shape) + ".txt", "w")
+    height = lowest - highest
+    width = rightmost - leftmost
+    y_center = (lowest + highest)/2
+    x_center = (rightmost + leftmost)/2
+    label.write("0 " + x_center.astype(str) + " " + y_center.astype(str) + " " + width.astype(str) + " " + height.astype(str))
+    label.close
+    
+    
+if __name__ == "__main__":
+    # creates images with different shapes and colors
+    
+    # colors
+    g_yellow = ["yellow", [0, 255, 255]]
+    g_white = ["white", [255,255,255]]
+    g_black = ["black", [0,0,0]]
+    g_red = ["red", [0,0, 255]]
+    g_blue = ["blue", [255,0,0]]
+    g_green = ["green", [0,128,0]]
+    g_purple = ["purple", [128,0,128]]
+    g_brown = ["brown", [0, 75, 150]]
+    g_orange = ["orange", [0,165,255]]
+    g_colors = [g_yellow, g_white, g_black, g_red, g_blue, g_green, g_purple, g_brown, g_orange]
+    
+    # shapes
+    g_shapes = ["triangle","rectangle","pentagon", "star"] # TODO: "circle", "semicircle", "quarter circle"
+    
+    g_size = 100  # size of the shape
+    g_background_path = "./src/model/resources/backgrounds/pavement3.jpg"
+    g_img = Image.open(g_background_path)
+    g_img = np.asarray(g_img)
+    
+    # initialize directory
+    os.chdir("./training_datasets/train/images")
+    
+    for g_color in g_colors:
+        for g_shape in g_shapes:
+            coord = (random.randint(0, np.size(g_img, 1)), random.randint(0, np.size(g_img, 0)))
+            print("center of the shape: ", coord)
+            g_img_with_shape = createImage(g_img, g_shape, coord, g_size, g_color, "A")  
+            # g_img_with_shape = Image.fromarray(np.uint8(g_img_with_shape))
+            # g_img_with_shape.show()  
+    
+    cv2.waitKey(0)
