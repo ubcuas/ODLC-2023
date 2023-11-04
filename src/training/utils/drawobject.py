a8ffd5a46b11b7f77f9aab824f300010ec754271
Tarit Witworrasakul <60599564+wtarit@users.noreply.github.com>
2023-11-04 16:28:01 -0700

    add text_color

--- src/training/utils/drawobject.py
+++ src/training/utils/drawobject.py
@@ -2,11 +2,11 @@ from .drawshape import *
 from .drawtext import *
 
 
-def draw_object(bg_img, shape, coord, size, color, text, font_size:float = 1.0):
+def draw_object(bg_img, shape, coord, size, shape_color, text_color, text, font_size:float = 1.0):
     """
     Place object (shape+text) on the image
     """
-    img, text_bbox, object_bbox = draw_rectangle(bg_img, coord, size, color)
-    img = putText(img, text, text_bbox, font_size)
+    img, text_bbox, object_bbox = draw_rectangle(bg_img, coord, size, shape_color)
+    img = putText(img, text, text_bbox, text_color, font_size)
     return img, object_bbox
     
