ef2145385f1bce5fb74142d6ba5bca083e021c98
Tarit Witworrasakul <60599564+wtarit@users.noreply.github.com>
2023-11-11 15:24:38 -0800

    add random case in generation

--- src/training/DataGeneration.py
+++ src/training/DataGeneration.py
@@ -3,53 +3,85 @@ import numpy as np
 from utils.drawobject import draw_object
 from utils.saveImgLabel import saveImageAndLabel
 from utils.imageAug import imageAugmentation
+import utils.commonDataStructure as cds
 import random
 import os
 import copy
-    
-def dataGeneration():
-    # creates images with different shapes and colors
-    
-    # colors
-    yellow = ["yellow", [0, 255, 255]]
-    white = ["white", [255,255,255]]
-    black = ["black", [0,0,0]]
-    red = ["red", [0,0, 255]]
-    blue = ["blue", [255,0,0]]
-    green = ["green", [0,128,0]]
-    purple = ["purple", [128,0,128]]
-    brown = ["brown", [0, 75, 150]]
-    orange = ["orange", [0,165,255]]
-    colors = [yellow, white, black, red, blue, green, purple, brown, orange]
-    
-    # shapes
-    shapes = ["triangle", "rectangle","pentagon", "star", "circle", "semicircle", "quarter circle"]
-    
-    size = 100  # size of the shape
-    background_path = "./src/training/resources/backgrounds/pavement3.jpg"
-    bg_img = cv2.imread(background_path)
-    
-    # initialize directory
-    os.chdir("./src/training/training_datasets/train/images")
-    
-    idx = 0
-    for color in colors:
-        for shape in shapes:
-            idx = 0
-            img = copy.deepcopy(bg_img)
-            coord = (random.randint(0, np.size(img, 1)), random.randint(0, np.size(img, 0)))
-            print("center of the shape: ", coord)
-            img_with_shape, bounding_box = draw_object(img, shape, coord, size, color[1], (0,165,255), "A")  
-            aug_imgs, new_bounding_boxes = imageAugmentation(img_with_shape, bounding_box)
-            for i in range(len(aug_imgs)):
-                print("i: ", i)
-                saveImageAndLabel(aug_imgs[i], shape, color, new_bounding_boxes[i],idx)
-                idx += 1
+from pathlib import Path
+import string
+import time
 
-    # cv2.imshow(img,img_with_shape)    
-    cv2.waitKey(0)
+
+def dataGeneration(bg_path: Path, output_path: Path):
+    # creates images with different shapes and colors
+
+    size = 100  # size of the shape
+    background_paths = list(bg_path.glob("*"))
+
+    # initialize directory
+    output_path.joinpath("images").mkdir(exist_ok=True, parents=True)
+    output_path.joinpath("labels").mkdir(exist_ok=True, parents=True)
+
+    idx = 0
+    total_img = 0
+    # characters = string.ascii_uppercase
+    characters = ["A"]
+    print(f"Number of Image being generated: {len(background_paths) * len(cds.colors) * (len(cds.colors) - 1) * 1 * 10}")
+    for bg in background_paths:
+        print(str(bg))
+        # bg_img = cv2.imread(str(bg))
+        bg_img = cv2.imread("src/training/resources/backgrounds/pavement.jpg")
+        print(bg_img.shape)
+        for color in cds.colors:
+            for shape in cds.shapes:
+                idx = 0
+                for text_color in cds.colors:
+                    if color == text_color:
+                        continue
+                    for char in characters:
+                        img = copy.deepcopy(bg_img)
+                        coord = (
+                            random.randint(int(size / 2), np.size(img, 1) - int(size / 2)),
+                            random.randint(int(size / 2), np.size(img, 0) - int(size / 2)),
+                        )
+                        print("center of the shape: ", coord)
+                        img_with_shape, bounding_box = draw_object(
+                            img, shape, coord, size, color[1], text_color[1], char
+                        )
+                        aug_imgs, new_bounding_boxes = imageAugmentation(
+                            img_with_shape, bounding_box
+                        )
+                        print(total_img)
+                        for i in range(len(aug_imgs)):
+                            saveImageAndLabel(
+                                aug_imgs[i], output_path, shape, color, new_bounding_boxes[i], idx
+                            )
+                            total_img += 1
+                            idx += 1
+
+    # cv2.imshow(img,img_with_shape)
+    # cv2.waitKey(0)
+
 
 if __name__ == "__main__":
-    dataGeneration()
-    
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--output", default="output/")
+    parser.add_argument(
+        "-i",
+        "--input",
+        default="backgroud_images/",
+        help="Backgroud image folder to put object on",
+    )
+    args = parser.parse_args()
+    output_path = Path(args.output)
+    output_path.mkdir(exist_ok=True, parents=True)
+    input_path = Path(args.input)
+    if not input_path.exists():
+        raise Exception("Input a valid input path")
+    start_t = time.time()
+    dataGeneration(input_path, output_path)
+    print(f"Total time to generate images: {time.time() - start_t}")
+
 print("end of DataGeneration")
--- src/training/utils/saveImgLabel.py
+++ src/training/utils/saveImgLabel.py
@@ -2,27 +2,26 @@ import cv2
 import os
 import numpy as np
 import utils.commonDataStructure as cds
+from pathlib import Path
 
 
-def saveImageAndLabel(img, shape, color, bounding_box, idx):
+def saveImageAndLabel(img, output_path: Path, shape, color, bounding_box, idx):
     """
     create training data image
     """
     # write image to the file
-    os.chdir("../images")  # change to the right directory
-    image_file = str(color[0]) + "_" + str(shape) + "_" + str(idx) + ".jpg"     # create image file
-    cv2.imwrite(image_file, img)
+    image_file = output_path.joinpath("images", str(color[0]) + "_" + str(shape) + "_" + str(idx) + ".jpg")     # create image file
+    cv2.imwrite(str(image_file), img)
 
     # create corresponding label file
-    os.chdir("../labels")
-    addLabel(bounding_box, color[0], shape, idx, np.size(img, 0), np.size(img, 1))
+    addLabel(bounding_box, color[0], output_path, shape, idx, np.size(img, 0), np.size(img, 1))
     
     return img
 
 
-def addLabel(bounding_box, color, shape, idx, img_height, img_width):
+def addLabel(bounding_box, color, output_path: Path, shape, idx, img_height, img_width):
     # add labels
-    label = open(str(color) + "_" + str(shape) + "_" + str(idx) + ".txt", "w")
+    label = open(output_path.joinpath("labels", str(color) + "_" + str(shape) + "_" + str(idx) + ".txt"), "w")
     height = (bounding_box[1][1] - bounding_box[0][1])/img_height
     width = (bounding_box[1][0] - bounding_box[0][0])/img_width
     y_center = ((bounding_box[1][1] + bounding_box[0][1])/2)/img_height
