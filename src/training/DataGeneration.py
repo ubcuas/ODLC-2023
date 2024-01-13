21580633195e198d5d898cdcc4fae6f8efd4f0bf
Tarit Witworrasakul <60599564+wtarit@users.noreply.github.com>
2024-01-13 10:58:23 -0800

    Generate multiple image

--- src/training/DataGeneration.py
+++ src/training/DataGeneration.py
@@ -4,8 +4,8 @@ from utils.drawobject import draw_object
 from utils.saveImgLabel import saveImageAndLabel
 from utils.imageAug import imageAugmentation
 import utils.commonDataStructure as cds
+from utils.commonDataStructure import ObjectLabel
 import random
-import math
 import os
 import copy
 from pathlib import Path
@@ -13,10 +13,21 @@ import string
 import time
 
 
-def dataGeneration(bg_path: Path, output_path: Path):
+def check_overlap(coord: tuple[int, int], placed_objs: list[ObjectLabel]):
+    if placed_objs:
+        for obj in placed_objs:
+            if obj.pt1[0] < coord[0] < obj.pt2[0]:
+                return True
+            if obj.pt1[1] < coord[1] < obj.pt2[1]:
+                return True
+    return False
+
+
+def dataGeneration(bg_path: Path, output_path: Path, num_gen: int):
     # creates images with different shapes and colors
 
-    size = 100  # size of the shape
+    size_min = 30  # size of the shape
+    size_max = 55
     background_paths = list(bg_path.glob("*"))
 
     # initialize directory
@@ -24,94 +35,105 @@ def dataGeneration(bg_path: Path, output_path: Path):
     output_path.joinpath("labels").mkdir(exist_ok=True, parents=True)
 
     idx = 0
-    total_img = 0
     num_augimg_per_original = 10
-    characters = cds.characters
-    print(
-        f"Number of Image being generated: {len(background_paths) * len(cds.shapes) * len(cds.colors) * (len(cds.colors) - 1) * (len(characters)) * num_augimg_per_original}"
-    )
-    
-    for color in cds.colors:
-        for shape in cds.shapes:
-            idx = 0
-            for bg in background_paths:
-                bg_img = cv2.imread(str(bg))
-                for text_color in cds.colors:
-                    if color == text_color:
-                        continue
-                    for char in characters:
-                        img = copy.deepcopy(bg_img)
-                        
-                        # # old coord generation code
-                        # coord = (
-                        #     random.randint(
-                        #         int(size / 2), np.size(img, 1) - int(size / 2)
-                        #     ),
-                        #     random.randint(
-                        #         int(size / 2), np.size(img, 0) - int(size / 2)
-                        #     ),
-                        # )
+    characters = string.ascii_uppercase
+    max_obj_per_img = 5
+    idx = 0
+    generated_objs = {}
+    # iterating over every background to minimize reading from disk
+    for bg in background_paths:
+        bg_img = cv2.imread(str(bg))
+        bg_img_height, bg_img_width, _ = bg_img.shape
+        # use all the background images equally
+        for _ in range(int(num_gen / len(background_paths))):
+            # pick number of object
+            num_obj = random.randint(1, max_obj_per_img)
+            img = copy.deepcopy(bg_img)
+            size = random.randint(size_min, size_max)
+            # place each object onto the image
+            placed_objs = []
+            for _ in range(num_obj):
+                shape = random.choice(cds.shapes)
+                shape_color = random.choice(cds.colors)
+                text_color = random.choice(cds.colors)
+                while text_color == shape_color:
+                    print("duplicate_color")
+                    text_color = random.choice(cds.colors)
+                char = random.choice(characters)
+                coord = (
+                    random.randint(int(size / 2), np.size(img, 1) - int(size / 2)),
+                    random.randint(int(size / 2), np.size(img, 0) - int(size / 2)),
+                )
+                if check_overlap(coord, placed_objs):
+                    print("overlapped")
+                    continue
+                img, bbox = draw_object(
+                    img, shape, coord, size, shape_color[1], text_color[1], char
+                )
+                obj = ObjectLabel(
+                    bbox[0],
+                    bbox[1],
+                    bg_img_width,
+                    bg_img_height,
+                    shape,
+                    shape_color,
+                    text_color,
+                )
+                placed_objs.append(obj)
+                if obj.name in generated_objs:
+                    generated_objs[obj.name] += 1
+                else:
+                    generated_objs[obj.name] = 1
 
-                        # generates coord so that it lies within a circle centered at the middle of the image
-                        # this makes it so that the shape won't go out-of-bounds if the image is rotated
-
-                        # radius of the circle
-                        if np.size(img, 1) >= np.size(img, 0):
-                            circle_r = (np.size(img, 0) - size)/2
-                        else:
-                            circle_r = (np.size(img, 1) - size)/2
-                        
-                        # center of the circle (x, y)
-                        circle_x = np.size(img, 1)/2
-                        circle_y = np.size(img, 0)/2
-
-                        # random angle
-                        alpha = 2 * math.pi * random.random()
-
-                        # random radius
-                        r = circle_r * math.sqrt(random.random())
-
-                        # calculating coordinates
-                        x_coord = r * math.cos(alpha) + circle_x
-                        y_coord = r * math.sin(alpha) + circle_y
-                        coord = (int(x_coord), int(y_coord))
-
-                        # print("center of the shape: ", coord)
-                        img_with_shape, bounding_box = draw_object(
-                            img, shape, coord, size, color[1], text_color[1], char
-                        )
-                        bg_img_height, bg_img_width, channels = bg_img.shape
-                        aug_imgs, new_bounding_boxes = imageAugmentation(
-                            img_with_shape, bounding_box, bg_img_height, bg_img_width,num_aug_imgs=num_augimg_per_original)
-                        for i in range(len(aug_imgs)):
-                            saveImageAndLabel(
-                                aug_imgs[i],
-                                output_path,
-                                shape,
-                                color,
-                                new_bounding_boxes[i],
-                                idx,
-                            )
-                            total_img += 1
-                            idx += 1
-                    print(total_img)
-
-    # cv2.imshow(img,img_with_shape)
-    # cv2.waitKey(0)
+            saveImageAndLabel(
+                img,
+                output_path,
+                placed_objs,
+                idx,
+            )
+            idx += 1
+            # aug_imgs, new_bounding_boxes = imageAugmentation(
+            #     img_with_shape,
+            #     bounding_box,
+            #     bg_img_height,
+            #     bg_img_width,
+            #     num_aug_imgs=num_augimg_per_original,
+            # )
+            # for i in range(len(aug_imgs)):
+            #     saveImageAndLabel(
+            #         aug_imgs[i],
+            #         output_path,
+            #         shape,
+            #         shape_color,
+            #         new_bounding_boxes[i],
+            #         idx,
+            #     )
+    print(generated_objs)
 
 
 if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
-    output_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "./training_datasets/valid")
+    output_path = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), "./training_datasets/train"
+    )
     parser.add_argument("-o", "--output", default=output_path)
-    input_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "./resources/backgrounds")
+    input_path = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), "./resources/backgrounds"
+    )
     parser.add_argument(
         "-i",
         "--input",
         default=input_path,
         help="Backgroud image folder to put object on",
+    )
+    parser.add_argument(
+        "-a",
+        "--amount",
+        type=int,
+        default=input_path,
+        help="Amount of image to generate",
     )
     args = parser.parse_args()
     output_path = Path(args.output)
@@ -120,7 +142,8 @@ if __name__ == "__main__":
     if not input_path.exists():
         raise Exception("Input a valid input path")
     start_t = time.time()
-    dataGeneration(input_path, output_path)
+    dataGeneration(input_path, output_path, args.amount)
     print(f"Total time to generate images: {time.time() - start_t}")
+
 
 print("end of DataGeneration")
--- src/training/utils/commonDataStructure.py
+++ src/training/utils/commonDataStructure.py
@@ -1,34 +1,135 @@
 # colors
-yellow = ["yellow", [0, 255, 255]]
-white = ["white", [255,255,255]]
-black = ["black", [0,0,0]]
-red = ["red", [0,0, 255]]
-blue = ["blue", [255,0,0]]
-green = ["green", [0,128,0]]
-purple = ["purple", [128,0,128]]
+white = ["white", [255, 255, 255]]
+black = ["black", [0, 0, 0]]
+red = ["red", [0, 0, 255]]
+blue = ["blue", [255, 0, 0]]
+green = ["green", [0, 128, 0]]
+purple = ["purple", [128, 0, 128]]
 brown = ["brown", [0, 75, 150]]
-orange = ["orange", [0,165,255]]
-colors = [yellow, white, black, red, blue, green, purple, brown, orange]
-# colors = [yellow, white]
-
-# valid characters
-characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-# characters = ["A", "B"]
+orange = ["orange", [0, 165, 255]]
+colors = [white, black, red, blue, green, purple, brown, orange]
 
 # shapes
-shapes = ["triangle", "rectangle","pentagon", "star", "circle", "semicircle", "quarter circle", "cross"]
-# shapes = ["triangle", "rectangle"]
+shapes = [
+    "triangle",
+    "rectangle",
+    "pentagon",
+    "star",
+    "circle",
+    "semicircle",
+    "quarter circle",
+    "cross",
+]
+# shapes = ["triangle", "quarter circle"]
 
-# lookup table to find index
-lookup_table = ['yellow_triangle', 'yellow_rectangle', 'yellow_pentagon', 'yellow_star', 'yellow_circle', 'yellow_semicircle', 'yellow_quarter circle', 'yellow_cross', 'white_triangle', 'white_rectangle', 'white_pentagon', 'white_star', 'white_circle', 'white_semicircle', 'white_quarter circle', 'white_cross', 'black_triangle', 'black_rectangle', 'black_pentagon', 'black_star', 'black_circle', 'black_semicircle', 'black_quarter circle', 'black_cross', 'red_triangle', 'red_rectangle', 'red_pentagon', 'red_star', 'red_circle', 'red_semicircle', 'red_quarter circle', 'red_cross', 'blue_triangle', 'blue_rectangle', 'blue_pentagon', 'blue_star', 'blue_circle', 'blue_semicircle', 'blue_quarter circle', 'blue_cross', 'green_triangle', 'green_rectangle', 'green_pentagon', 'green_star', 'green_circle', 'green_semicircle', 'green_quarter circle', 'green_cross', 'purple_triangle', 'purple_rectangle', 'purple_pentagon', 'purple_star', 'purple_circle', 'purple_semicircle', 'purple_quarter circle', 'purple_cross', 'brown_triangle', 'brown_rectangle', 'brown_pentagon', 'brown_star', 'brown_circle', 'brown_semicircle', 'brown_quarter circle', 'brown_cross', 'orange_triangle', 'orange_rectangle', 'orange_pentagon', 'orange_star', 'orange_circle', 'orange_semicircle', 'orange_quarter circle', 'orange_cross']
-# lookup_table = ['yellow_triangle', 'yellow_rectangle', 'white_triangle', 'white_rectangle']
+lookup_table = [
+    "white_triangle",
+    "white_rectangle",
+    "white_pentagon",
+    "white_star",
+    "white_circle",
+    "white_semicircle",
+    "white_quarter circle",
+    "white_cross",
+    "black_triangle",
+    "black_rectangle",
+    "black_pentagon",
+    "black_star",
+    "black_circle",
+    "black_semicircle",
+    "black_quarter circle",
+    "black_cross",
+    "red_triangle",
+    "red_rectangle",
+    "red_pentagon",
+    "red_star",
+    "red_circle",
+    "red_semicircle",
+    "red_quarter circle",
+    "red_cross",
+    "blue_triangle",
+    "blue_rectangle",
+    "blue_pentagon",
+    "blue_star",
+    "blue_circle",
+    "blue_semicircle",
+    "blue_quarter circle",
+    "blue_cross",
+    "green_triangle",
+    "green_rectangle",
+    "green_pentagon",
+    "green_star",
+    "green_circle",
+    "green_semicircle",
+    "green_quarter circle",
+    "green_cross",
+    "purple_triangle",
+    "purple_rectangle",
+    "purple_pentagon",
+    "purple_star",
+    "purple_circle",
+    "purple_semicircle",
+    "purple_quarter circle",
+    "purple_cross",
+    "brown_triangle",
+    "brown_rectangle",
+    "brown_pentagon",
+    "brown_star",
+    "brown_circle",
+    "brown_semicircle",
+    "brown_quarter circle",
+    "brown_cross",
+    "orange_triangle",
+    "orange_rectangle",
+    "orange_pentagon",
+    "orange_star",
+    "orange_circle",
+    "orange_semicircle",
+    "orange_quarter circle",
+    "orange_cross",
+]
 
-# # To generate data.yaml and lookup_table
-# a = 0
-# for color in colors:
-#     for shape in shapes:
-#         # print(str(a) + ": " + color[0] + "_" + shape)
-#         name_color_shape = color[0] + "_" + shape
-#         lookup_table.append(name_color_shape)
-#         a += 1
-# print(lookup_table)
+
+class ObjectLabel:
+    def __init__(
+        self,
+        pt1: tuple[int, int],
+        pt2: tuple[int, int],
+        imgw: int,
+        imgh: int,
+        shape: str,
+        shape_color: tuple[str, tuple[int, int, int]],
+        text_color: tuple[str, tuple[int, int, int]],
+    ) -> None:
+        self.pt1 = pt1
+        self.pt2 = pt2
+        self.imgw = imgw
+        self.imgh = imgh
+        self.shape = shape
+        self.shape_color = shape_color
+        self.text_color = text_color
+        self.name = f"{shape}_{shape_color[0]}"
+
+    def get_normalized_centerx(self) -> float:
+        return ((self.pt1[0] + self.pt2[0]) / 2) / self.imgw
+
+    def get_normalized_centery(self) -> float:
+        return ((self.pt1[1] + self.pt2[1]) / 2) / self.imgh
+
+    def get_normalized_width(self) -> float:
+        return (self.pt2[0] - self.pt1[0]) / self.imgw
+
+    def get_normalized_height(self) -> float:
+        return (self.pt2[1] - self.pt1[1]) / self.imgh
+
+
+if __name__ == '__main__':
+    # To generate data.yaml and lookup_table
+    a = 0
+    for color in colors:
+        for shape in shapes:
+            print(str(a) + ": " + color[0] + "_" + shape)
+            name_color_shape = color[0] + "_" + shape
+            lookup_table.append(name_color_shape)
+            a += 1
+    print(lookup_table)
