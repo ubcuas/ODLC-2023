4210996770b3e949ea386a388321cee636bb63c4
Richard Han <rrhan2002@gmail.com>
2024-06-12 00:07:15 -0700

    It works, added dead reckoning and visualization util with gps points

--- src/predict/convert_coordinate.py
+++ src/predict/convert_coordinate.py
@@ -3,7 +3,104 @@ import commonDataStructure as cds
 from pyproj import Geod
 
 
+def pixel_to_gps(pixel_x, pixel_y, heading, latitude, longitude, altitude):
+    """
+    Calculates the GPS location of a pixel
 
+    @param pixel_x: X-axis coordinate, 0 at left
+    @param pixel_y: Y-axis coordinatem 0 at top
+    @param heading: Azimuth starting at north going clockwise
+    @param longitude: Longitude from telemetry data
+    @param latitude: Latitude from telemetry data
+    @param altitude: Altitude in meters from telemetry data
+    @return: (Longitude, latitude) of pixel
+    """
+    # in pixels
+    image_w = 5472
+    image_h = 3648
+    center_x = image_w // 2
+    center_y = image_h // 2
+
+    # in cm
+    sensor_w = 1.31328
+
+    # sensor_h = 0.87552
+    focal_length = 1.2
+
+    # convert meters to centimeters
+    altitude *= 100
+
+    # ground sample distance cm/pixel
+    gsd = (altitude * sensor_w) / (focal_length * image_w)
+
+    # distance from center in pixels (left of center negative, below center negative y)
+    dist_x = pixel_x - center_x
+    dist_y = center_y - pixel_y
+
+    # distance from center in cm
+    dist_x *= gsd
+    dist_y *= gsd
+
+    # distance from center in m
+    dist_x /= 100
+    dist_y /= 100
+
+    # rotate point according to heading
+    heading_rad = heading * np.pi / 180
+    rotation_matrix = np.array(
+        [[np.cos(heading_rad), np.sin(heading_rad)],
+         [-np.sin(heading_rad), np.cos(heading_rad)]]
+    )
+    dist_east, dist_north = rotation_matrix @ [[dist_x], [dist_y]]
+
+    # add distances to current gps
+    geod = Geod(ellps="WGS84")
+    longitude, latitude, _ = geod.fwd(lons=longitude, lats=latitude, az=90, dist=dist_east)
+    longitude, latitude, _ = geod.fwd(lons=longitude, lats=latitude, az=0, dist=dist_north)
+
+    return latitude, longitude
+
+
+def add_dead_reckoning(heading, speed, latitude, longitude, telemetry_time, image_time, weight):
+    """
+    Estimates current position from speed. Assumes speed has been constant between image and telemetry time
+
+    @param heading: Azimuth starting at north going clockwise
+    @param speed: Speed in m/s from telemetry data
+    @param latitude: Latitude from telemetry data
+    @param longitude: Longitude from telemetry data
+    @param telemetry_time: Timestamp of telemetry data in milliseconds
+    @param image_time: Timestamp of image in milliseconds
+    @param weight: Strength of dead reckoning estimation
+    @return: Estimated (longitude, latitude)
+    """
+    geod = Geod(ellps="WGS84")
+
+    delta_t = telemetry_time - image_time
+    displacement = (delta_t / 1000) * speed
+    displacement *= weight
+    longitude, latitude, _ = geod.fwd(lons=longitude, lats=latitude, az=heading, dist=displacement)
+
+
+def print_gps_corners(heading, altitude, latitude, longitude):
+    """
+    Prints GPS positions of the corners of the area covered by camera
+
+    @param heading: Azimuth starting at north going clockwise
+    @param altitude: Altitude in meters from telemetry data
+    @param latitude: Latitude from telemetry data
+    @param longitude: Longitude from telemetry data
+    """
+    top_left = pixel_to_gps(0, 0, heading, latitude, longitude, altitude)
+    top_right = pixel_to_gps(5472, 0, heading, latitude, longitude, altitude)
+    bottom_right = pixel_to_gps(5472, 3648, heading, latitude, longitude, altitude)
+    bottom_left = pixel_to_gps(0, 3648, heading, latitude, longitude, altitude)
+    print(f"{top_left[0]}, {top_left[1]}")
+    print(f"{top_right[0]}, {top_right[1]}")
+    print(f"{bottom_right[0]}, {bottom_right[1]}")
+    print(f"{bottom_left[0]}, {bottom_left[1]}")
+
+print_gps_corners(20, 30, 49.260605, -123.245995)
 # def R(theta):
 #     """
 #     Rotation matrix of theta degrees counterclockwise.
@@ -13,8 +110,8 @@ from pyproj import Geod
 #
 #     return np.array([[np.cos(theta), -np.sin(theta)],
 #                      [np.sin(theta), np.cos(theta) ]])
-    
-    
+
+
 # def imgCoordToGPS(obj_label: cds.ObjectLabel, drone_info: cds.DroneInfo):
 #     """
 #     Convert coordinates of an object on an image to coordinates on the GPS.
@@ -57,80 +154,3 @@ from pyproj import Geod
 #     x_3, y_3 = [x_2 + GPS_x, y_2 + GPS_y]
 #
 #     return [x_3, y_3]
-
-
-def pixel_to_gps(pixel_x, pixel_y, heading, speed, longitude, latitude, altitude, telemetry_time, image_time):
-    """
-    Calculates the GPS location of a pixel
-
-    @param pixel_x: X-axis coordinate, 0 at left
-    @param pixel_y: Y-axis coordinatem 0 at top
-    @param heading: Azimuth starting at north going clockwise
-    @param speed: Speed in m/s from telemetry data
-    @param longitude: Longitude from telemetry data
-    @param latitude: Latitude from telemetry data
-    @param altitude: Altitude in meters from telemetry data
-    @param telemetry_time: Timestamp of telemetry data in milliseconds
-    @param image_time: Timestamp of image in milliseconds
-    @return: (Longitude, latitude) of pixel
-    """
-    # in pixels
-    image_w = 5472
-    image_h = 3648
-
-    center_x = image_w // 2
-    center_y = image_h // 2
-
-    # in cm
-    sensor_w = 1.31328
-    # sensor_h = 0.87552
-    focal_length = 1.2
-
-    # convert meters to centimeters
-    altitude *= 100
-
-    # ground sample distance cm/pixel
-    gsd = (altitude * sensor_w) / (focal_length * image_w)
-
-    # distance from center in pixels
-    dist_x = pixel_x - center_x
-    dist_y = center_y - pixel_y
-
-    # distance from center in cm
-    dist_x *= gsd
-    dist_y *= gsd
-
-    # distance from center in m
-    dist_x /= 100
-    dist_y /= 100
-
-    dead_reckoning_weight = 1
-
-    delta_t = telemetry_time - image_time
-    displacement = (delta_t / 1000) * speed
-    displacement *= dead_reckoning_weight
-
-    geod = Geod(ellps="WGS84")
-    new_lon, new_lat, _ = geod.fwd(lons=longitude, lats=latitude, az=heading, dist=displacement)
-
-    heading_rad = heading * np.pi / 180
-    rotation_matrix = np.array(
-        [[np.cos(heading_rad), -np.sin(heading_rad)],
-        [np.sin(heading_rad), np.cos(heading_rad)]]
-    )
-    dist_east, dist_north = rotation_matrix @ [[dist_x], [dist_y]]
-
-    new_lon, new_lat, _ = geod.fwd(lons=new_lon, lats=new_lat, az=90, dist=dist_east)
-    new_lon, new_lat, _ = geod.fwd(lons=new_lon, lats=new_lat, az=0, dist=dist_north)
-
-    return new_lon, new_lat
-
-
-
-
-
-
-
-
-
-
